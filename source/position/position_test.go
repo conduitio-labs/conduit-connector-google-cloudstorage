09666e9a64bca5c81f721242b1da9fead4ab70a4
Lovro Mažgon <lovro.mazgon@gmail.com>
2024-06-28 15:36:21 +0200

    clean up code and CI actions

--- source/position/position_test.go
+++ source/position/position_test.go
@@ -22,7 +22,7 @@ import (
 )
 
 func Test_ParseRecordPosition(t *testing.T) {
-	var positionTests = []struct {
+	positionTests := []struct {
 		name    string
 		wantErr bool
 		in      sdk.Position
@@ -85,7 +85,7 @@ func Test_ParseRecordPosition(t *testing.T) {
 }
 
 func Test_ConvertSnapshotPositionToCDC(t *testing.T) {
-	var positionTests = []struct {
+	positionTests := []struct {
 		name    string
 		wantErr bool
 		in      sdk.Position
